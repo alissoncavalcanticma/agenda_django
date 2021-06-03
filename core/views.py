d771339cb004c9910c4daf0fe956e9ca17dd02a2
Alisson C <alissoncavalcanticma@gmail.com>
2021-06-03 20:51:53 -0300

    criando templates

--- core/views.py
+++ core/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from core.models import Evento
 
 # Create your views here.
+
+def lista_eventos(request):
+    #evento = Evento.objects.get(id=2)
+    #response = {'evento':evento}
+    #return render(request, 'agenda.html', response)
+    evento = Evento.objects.all()
+    dados = {'evento':evento}
+    return render(request, 'agenda.html', dados)
+
+# def index(request):
+#     return redirect('/agenda')
